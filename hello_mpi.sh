77fa5d578146ab2f21afbb67c2c7697b945dd457
José Abell <jaabell@uandes.cl>
2022-05-20 12:09:58 -0400

    batch slurm script for hello mpi

---
+++ hello_mpi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=mpi-hello    # Job name
+#SBATCH --ntasks=8                    # Run on a single CPU
+#SBATCH --output=serial_test_%j.log   # Standard output and error log
+pwd; hostname; date
+
+mpirun python hello-mpi.py
+
+date
