e2595873a6ff630b50be9afac9385d2957d55b9a
José Abell <jaabell@uandes.cl>
2022-06-07 10:02:15 -0400

    more node

--- run-hello-mpi.sh
+++ run-hello-mpi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=hello-mpi    # Job name
-#SBATCH --ntasks=8                    # Run on a single CPU
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=16                   # Run on a single CPU
 #SBATCH --output=hellompi_%j.log   # Standard output and error log
 pwd; hostname; date
 
