3a803dcd6b89b86ca9e597f120e73844c8194ef7
José Abell <jaabell@uandes.cl>
2022-06-07 09:27:31 -0400

    Test bcast

--- run-test-bcast.sh
+++ run-test-bcast.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=shakermakertest    # Job name
+#SBATCH --job-name=test-bcast    # Job name
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=16
 #SBATCH --output=log-test-bcast-%j.log   # Standard output and error log
--- test-bcast.py
+++ test-bcast.py
@@ -1,17 +1,38 @@
 from mpi4py import MPI
 import numpy as np
+from time import perf_counter
+
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-if rank == 0:
-    data = np.arange(100, dtype='i')
-else:
-    data = np.empty(100, dtype='i')
-comm.Bcast(data, root=0)
-for i in range(100):
-    assert data[i] == i
+
+check = False
+
+sizes_of_sends = [100, 1000, 10000, 1000000]
+
+time_taken = []
+
+for sz in sizes_of_sends:
+
+    if rank == 0:
+        data = np.arange(sz, dtype='i')
+    else:
+        data = np.empty(sz, dtype='i')
+
+    t1 = perf_counter()
+    comm.Bcast(data, root=0)
+    t2 = perf_counter()
+
+    if check:
+        for i in range(sz):
+            assert data[i] == i
+
+
+    if rank == 0:
+        with open("test-bcast-size-{size}.txt","a") as fid:
+            fid.write(f"{sz} {t2-t1}\n")
 
 
 
